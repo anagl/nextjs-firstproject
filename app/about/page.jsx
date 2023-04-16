06aed0860c91ec157cbae64904874b1b13f96e75
Ana Garcia <alapole@gmail.com>
2023-04-16 17:21:44 +0200

    Subida proyecto Nextjs

---
+++ app/about/page.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+        aspernatur atque, quos fuga, neque, omnis nemo magnam soluta culpa rerum
+        aperiam reiciendis dolorem hic suscipit numquam labore repellat
+        temporibus in fugiat illo facere modi vitae? Voluptates labore,
+        praesentium maxime sit amet ullam vitae quas et eum beatae earum sunt
+        officia obcaecati enim, iure eveniet. Vero, perspiciatis placeat
+        architecto ipsum, minima dolor earum rem debitis id corrupti similique
+        cupiditate deleniti dicta sit, inventore ipsam? Quaerat, sequi unde
+        reiciendis nam quis dolore sed autem, pariatur accusamus natus, modi
+        iusto! Ducimus, aperiam! Minima cumque dolorem similique animi quia
+        deleniti recusandae aspernatur corporis dolore.
+      </p>
+    </div>
+  );
+}
+
+export default AboutPage;
---
+++ app/services/page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function ServicesPage() {
+  return (
+    <div>
+      <h1>Services</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque fuga
+        explicabo dolores optio voluptates odit qui reiciendis obcaecati,
+        nesciunt dicta, placeat deserunt culpa enim ea voluptatibus officiis
+        sit, est at dolore exercitationem. Molestiae itaque ea, magnam tenetur
+        animi rem? Sequi quas harum at nisi doloremque iusto eaque rem
+        voluptatibus cumque!
+      </p>
+    </div>
+  );
+}
+
+export default ServicesPage;
